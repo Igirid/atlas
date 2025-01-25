957627f84efc71e8a05dba4a36a3829ca859d4f9
David Igiri <igiridavd2@gmail.com>
2025-01-25 13:23:48 +0100

    deploying images

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export", // <=== enables static exports
   reactStrictMode: true,
-  basePath: "/atlas"
+  // basePath: "/atlas"
 };
 
 export default nextConfig;
